fd5d600a4b91549b395200b6f2db6a96476772d1
Imane123-ui <60091739+Imane123-ui@users.noreply.github.com>
2020-06-19 13:05:27 +0200

    Update models.py
    Problème résolu pour avoir des strings en retour et non tuple (problème affichage)

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+ 
+
 class Place(models.Model):
     ID_Place = models.AutoField(primary_key=True)
     Name_Place = models.CharField(max_length=100)
@@ -9,28 +11,35 @@ class Place(models.Model):
     Rating_Place = models.CharField(max_length=10)
     Photo_Place = models.CharField(max_length=500)
     Link_Place = models.CharField(max_length=500)
-    Description_Place = models.TextField(max_length=2000)
+    Description_Place = models.CharField(max_length=2000)
     Price_Place = models.CharField(max_length=50)
     City_Place = models.CharField(max_length=50)
+    Phone_Place = models.CharField(max_length=50)
     Meteo1_Place = models.CharField(max_length=50)
     Meteo2_Place = models.CharField(max_length=50)
     Meteo3_Place = models.CharField(max_length=50)
     Meteo4_Place = models.CharField(max_length=50)
     Meteo5_Place = models.CharField(max_length=50)
     def __str__(self):
-        return self.ID_Place, self.Name_Place, self.Address_Place, self.Latitude_Place, self.Longitude_Place, self.Rating_Place, self.Photo_Place, self.Link_Place, self.Description_Place,self.Price_Place, self.City_Place, self.Meteo1_Place, self.Meteo2_Place, self.Meteo3_Place, self.Meteo4_Place, self.Meteo5_Place
+        return '%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s'%(self.ID_Place, self.Name_Place, self.Address_Place, self.Latitude_Place, self.Longitude_Place, self.Rating_Place, self.Photo_Place, self.Link_Place, self.Description_Place,self.Price_Place, self.City_Place, self.Phone_Place, self.Meteo1_Place, self.Meteo2_Place, self.Meteo3_Place, self.Meteo4_Place, self.Meteo5_Place)
+
+ 
 
 class Category(models.Model):
     ID_Category = models.AutoField(primary_key=True)
     Name_Category = models.CharField(max_length=100)
     def __str__(self):
-        return self.ID_Category, self.Name_Category
+        return '%s %s'%(self.ID_Category, self.Name_Category)
+
+ 
 
 class Type(models.Model):
     ID_Type = models.AutoField(primary_key=True)
     Name_Type = models.CharField(max_length=100)
     def __str__(self):
-        return self.ID_Type, self.Name_Type
+        return '%s %s'%(self.ID_Type, self.Name_Type)
+
+ 
 
 class User(models.Model):
     ID_User = models.AutoField(primary_key=True)
@@ -46,7 +55,9 @@ class User(models.Model):
     Password_User = models.CharField(max_length=50)
     Visited_User = models.CharField(max_length=50)
     def __str__(self):
-        return self.ID_User, self.Name_User, self.Surname_User, self.Birthdate_User, self.Address_User, self.Latitude_User, self.Longitude_User, self.City_User, self.Email_User, self.Phone_User, self.Password_User, self.Visited_User
+        return '%s %s %s %s %s %s %s %s %s %s %s %s'%(self.ID_User, self.Name_User, self.Surname_User, self.Birthdate_User, self.Address_User, self.Latitude_User, self.Longitude_User, self.City_User, self.Email_User, self.Phone_User, self.Password_User, self.Visited_User)
+
+ 
 
 class Weather(models.Model):
     ID_Weather = models.AutoField(primary_key=True)
@@ -57,10 +68,12 @@ class Weather(models.Model):
     Longitude_Weather = models.CharField(max_length=10)
     Date_Weather = models.DateField()
     def __str__(self):
-        return self.ID_Weather, self.Temperature_Weather, self.Weather_Weather, self.City_Weather, self.Latitude_Weather, self.Longitude_Weather, self.Date_Weather
+        return '%s %s %s %s %s %s %s'%(self.ID_Weather, self.Temperature_Weather, self.Weather_Weather, self.City_Weather, self.Latitude_Weather, self.Longitude_Weather, self.Date_Weather)
+
+ 
 
 class Hobby(models.Model):
     ID_Hobby = models.AutoField(primary_key=True)
     Name_Hobby = models.CharField(max_length=100)
     def __str__(self):
-        return self.ID_Hobby, self.Name_Hobby
+        return '%s %s'%(self.ID_Hobby, self.Name_Hobby)
